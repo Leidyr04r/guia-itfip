1ee76661d04141935de81bebd00935f5d9ccf922
Leidyr04r <carolrodri645@gmail.com>
2024-09-15 15:08:32 -0500

    Add files via upload

---
+++ web1/public/menu.php
@@ -0,0 +1,38 @@
+ <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="videos.php">
+                <svg class="bi"><use xlink:href="#house-fill"/></svg>
+                Tus videos
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="peliculas.php">
+                <svg class="bi"><use xlink:href="#file-earmark"/></svg>
+                Tus peliculas
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="tiempo.php">
+                <svg class="bi"><use xlink:href="#cart"/></svg>
+                Tiempo de reproduccion
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="#">
+                <svg class="bi"><use xlink:href="#people"/></svg>
+                
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="#">
+                <svg class="bi"><use xlink:href="#graph-up"/></svg>
+                
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center gap-2" href="#">
+                <svg class="bi"><use xlink:href="#puzzle"/></svg>
+                
+              </a>
+            </li>
+          </ul>
